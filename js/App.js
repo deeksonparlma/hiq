64eef2bac6ea2bc2aeec00d362d0b01228346fab
deeksonparlma <dicksonparlma@gmail.com>
2019-10-16 17:03:48 +0300

    Added a login form

--- js/App.js
+++ js/App.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+  document.getElementById("showForm").onclick =function(){showForm()};
+
   $(".ham").click(function() {
    $(".options").slideToggle();
    $(".nav").css("opacity", "1");
   });
+
+  $(".close").click(function(){
+    $(".ff").slideUp();
+    $(".form").slideUp();
+  });
+  function showForm() {
+    $(".ff").show();
+    $(".form").slideDown();
+  }
   $(window).scroll(function(){
     if ($(document).scrollTop() > 50) {
             $(".nav").css("opacity", "0.6");
