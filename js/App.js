8aa35f4c95b26b81bca98f7d739bfbb43d84a6b5
deeksonparlma <dicksonparlma@gmail.com>
2019-10-16 11:31:58 +0300

    prototype 2

--- js/App.js
+++ js/App.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   $(".ham").click(function() {
    $(".options").slideToggle();
+   $(".nav").css("opacity", "1");
   });
+  $(window).scroll(function(){
+    if ($(document).scrollTop() > 50) {
+            $(".nav").css("opacity", "0.6");
+          } else {
+            $(".nav").css("opacity", "1");
+          }
+})
 });
